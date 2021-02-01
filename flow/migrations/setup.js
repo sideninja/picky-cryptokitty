880a1cfa11cb0538b09f6bc1697055789bc2464b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-02-01 10:46:31 +0100

    setup flow for dapp

---
+++ flow/migrations/setup.js
@@ -0,0 +1,43 @@
+const deployer = require('./deployer');
+const config = require('config');
+const Network = require('../lib/network');
+
+async function init() {
+
+  const network = new Network({ node: config.get("flow.network") });
+
+  // main account from flow emulator
+  const mainAccount = deployer.getAccount({
+    address: config.get("accounts.main.address"),
+    network
+  });
+
+  await deployer.createAccountAndDeploy(
+    'FungibleToken',
+    mainAccount,
+    network
+  );
+
+  await deployer.createAccountAndDeploy(
+    'NonFungibleToken', 
+    mainAccount, 
+    network
+  );
+
+  await deployer.createAccountAndDeploy(
+    'Kitty', 
+    mainAccount, 
+    network
+  );
+  
+  await deployer.createAccountAndDeploy(
+    'HairBall', 
+    mainAccount, 
+    network
+  );
+
+
+}
+
+
+module.exports = { init };
